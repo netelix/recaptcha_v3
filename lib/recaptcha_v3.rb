641f30da9ab4448c51d0ec4824504ac73d3407bd
Alexis Panet <netelix@gmail.com>
2021-02-11 16:04:07 +0100

    assets

--- lib/recaptcha_v3.rb
+++ lib/recaptcha_v3.rb
@@ -2,5 +2,12 @@ require "recaptcha_v3/version"
 require "recaptcha_v3/engine"
 
 module RecaptchaV3
-  
+  module Controllers
+    autoload :Helpers,        'recaptcha_v3/controllers/helpers'
+  end
+
+  require 'recaptcha_v3/helper'
+  ActiveSupport.on_load(:action_view) do
+    include RecaptchaV3::Helper
+  end
 end
