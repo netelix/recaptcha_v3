57448594a5d048be2fc68d3e9a7a837b3010217b
Alexis Panet <netelix@gmail.com>
2021-02-25 09:59:06 +0100

    lib/

--- lib/recaptcha_v3/helper.rb
+++ lib/recaptcha_v3/helper.rb
@@ -1,7 +1,7 @@
 module RecaptchaV3
   module Helper
     def recaptcha_v3
-      attributes = "data-recaptcha-client-id='<%= ENV['GOOGLE_CAPTCHA_CLIENT'] %>'"
+      attributes = "data-recaptcha-client-id='#{ENV['GOOGLE_CAPTCHA_CLIENT']}'"
       attributes += " data-should-challenge-recaptcha=1" if should_challenge_captcha?
       return attributes
     end
