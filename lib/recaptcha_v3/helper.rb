41fecbb0f4dadc8753f1c176039a93963eda4654
Alexis Panet <netelix@gmail.com>
2021-02-25 09:53:55 +0100

    remove client id from js

--- lib/recaptcha_v3/helper.rb
+++ lib/recaptcha_v3/helper.rb
@@ -1,7 +1,9 @@
 module RecaptchaV3
   module Helper
     def recaptcha_v3
-      return "data-should-challenge-recaptcha=1" if should_challenge_captcha?
+      attributes = "data-recaptcha-client-id='<%= ENV['GOOGLE_CAPTCHA_CLIENT'] %>'"
+      attributes += " data-should-challenge-recaptcha=1" if should_challenge_captcha?
+      return attributes
     end
   end
 end
